c0e7e3473dd9d7dd7a283d60956722256221ac7d
MahmoudRizk <mahmoudahmedrizk@gmail.com>
2017-07-04 10:37:18 +0200

    Implementing news widget (#4)

---
+++ alfred_news/alfred_news_widget.py
@@ -0,0 +1,37 @@
+from alfred.modules.api.a_base_widget import ABaseWidget
+from alfred.modules.api.view_components.a_paragraph import AParagraph
+from alfred.modules.api.view_components.a_href import AHref
+from .alfred_news import AlfredNews
+import feedparser
+
+
+class AlfredNewsWidget(ABaseWidget):
+    def __init__(self):
+        ABaseWidget.__init__(self)
+        self.title = ''
+        self.summary = ''
+        self.date =  ''
+        self.url = ''
+        self.image = ''
+
+    def callback(self):
+        feed = feedparser.parse("http://rss.cnn.com/rss/edition.rss")
+        for entry in feed['entries']:
+            self.title = entry.get('title', '')
+            self.summary = entry.get('summary', '')
+            self.date = entry.get('updated', '')
+            self.url = entry.get('link', '')
+            try:
+                self.image = entry.get('media_content', '')[0]['url']  # CNN
+            except:
+                pass
+
+            break
+
+    def construct_view(self):
+        self.title = self.title
+        self.content.append(AParagraph(self.summary))
+        self.content.append(AParagraph(self.date))
+        self.content.append(AParagraph(AHref(url = self.date, link = 'Read')))
+        self.image_url = self.image
+        self.title_on_image = True
